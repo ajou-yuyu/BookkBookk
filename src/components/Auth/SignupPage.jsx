ddf842020af8c88461c09c5a473779a884db3855
yuminnnnni <ka09023@ajou.ac.kr>
2024-05-23 17:45:57 +0900

    mypage

--- src/components/Auth/SignupPage.jsx
+++ src/components/Auth/SignupPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import '../../styles/signup.css';
+import MyPage from "../MyPage/MyPage";
 
 const SignupPage = () => {
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
@@ -10,29 +13,67 @@ const SignupPage = () => {
   };
 
   return (
-    <div>
-      <h2>회원가입</h2>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="이메일"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="비밀번호"
-      />
-      <input
-        type="password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        placeholder="비밀번호 확인"
-      />
-      <button onClick={handleSignup}>회원가입</button>
+    <div className="container">
+      <div className="background-box"></div>
+
+      <div className="logo">Bookk<br />Bookk</div>
+
+      <div className="input-container" style={{ left: 300, top: 274 }}>
+        <div className="input-box"></div>
+        <input
+          type="text"
+          className="input-field"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="이름"
+        />
+        {/* <MyPage name={name} /> */}
+        <div className="label" style={{ left: 51.66, top: 14 }}>이름</div>
+      </div>
+
+      <div className="input-container" style={{ left: 300, top: 378 }}>
+        <div className="input-box"></div>
+        <input
+          type="text"
+          className="input-field"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder="아이디"
+        />
+        <div className="label" style={{ left: 51.66, top: 14 }}>아이디</div>
+      </div>
+      {/* <MyPage name={name} /> => 그냥 여기있는 name을 MyPage로 전달하고 싶었던 건데 왜 signuppage에 흉측하게 뜨는 거냐며!~~~ 어떻게 하면 화면에 안뜨고 props로 전달만 하게 할까나? */}
+      <div className="input-container" style={{ left: 300, top: 493 }}>
+        <div className="input-box"></div>
+        <input
+          type="password"
+          className="input-field"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호"
+        />
+        <div className="label" style={{ left: 51.66, top: 14 }}>비밀 번호</div>
+      </div>
+
+      <div className="input-container" style={{ left: 300, top: 609 }}>
+        <div className="input-box"></div>
+        <input
+          type="password"
+          className="input-field"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="비밀번호 확인"
+        />
+        <div className="label" style={{ left: 52, top: 14 }}>비밀 번호 확인</div>
+      </div>
+
+      <div className="signup-button" style={{ left: 447, top: 724 }} onClick={handleSignup}>
+        <div className="signup-button-text">가입하기</div>
+      </div>
     </div>
   );
 };
 
 export default SignupPage;
+
+
--- src/components/MyPage/MyPage.jsx
+++ src/components/MyPage/MyPage.jsx
@@ -1,5 +1,29 @@
 import React, { useState } from "react";
+import '../../styles/mypage.css'
 
-const MyPage = () => {};
+const MyPage = (props) => {
+
+  const name = props.name;
+  //const { name } = props;
+
+  return (
+    <div className="container">
+      <div className="navbar">
+        <nav>
+          <ul>
+            <li><a href="#">로그아웃</a></li>
+            <li><a href="/mypage">MY PAGE</a></li>
+            <li><a href="/home">Bookk<br />Bookk</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div classname="welcome-message">{name}님 하이루!</div>
+      <div className="button-container">
+        <a href="/bucketlist"><button>버킷리스트</button></a>
+        <a href="/reviewlist"><button>독후감</button></a>
+      </div>
+    </div>
+  );
+};
 
 export default MyPage;
